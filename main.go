6e38cbd4380d3af02745f82698d6a5cba96fc5f8
Joaquin Nicolas <joako.n96@gmail.com>
2018-12-06 12:19:56 -0300

    Golang add support to cors

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"flag"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 const api = "https://api.iextrading.com/1.0"
 
 func main() {
+
+	port := flag.String("port", "8080", "server port")
+	flag.Parse()
+	
 	router := gin.Default()
+	router.Use(cors.Default())
 
 	// Serve react build files
 	router.Use(static.Serve("/", static.LocalFile("./build", true)))
@@ -18,10 +26,6 @@ func main() {
 	api.GET("/stocks", StockHandler)
 	
 	// start and run the server
-	router.Run(":3000")
+	router.Run(fmt.Sprintf(":%s", *port))
+
 }
-
-
-
-
-// https://api.iextrading.com/1.0/stock/aapl/chart/1m
