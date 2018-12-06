2e757e434750b4c9094818f748403e03ea8856c0
Joaquin Nicolas <joako.n96@gmail.com>
2018-12-06 12:22:08 -0300

    React bug fixes

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 
 export const Appbar = () => {
-  return <Navbar color="light" light>
+  return (
+    <Navbar color="light" light>
       <NavbarBrand href="/">
-        <a class="block border-box left relative z1 bg-clip-content logo fill-orange bg-light-orange" href="/">
-          <svg class="align-middle" viewBox="0 0 5 5" width="28" height="28">
-            <title>IEX home</title>
-            <path d="M0,0v5h5V0z M1,1h3v3H2V3H1z" />
-          </svg>
-        </a>
+        <svg className="align-middle" viewBox="0 0 5 5" width="28" height="28">
+          <title>IEX home</title>
+          <path d="M0,0v5h5V0z M1,1h3v3H2V3H1z" />
+        </svg>
       </NavbarBrand>
-    </Navbar>;
+    </Navbar>
+  );
 };
