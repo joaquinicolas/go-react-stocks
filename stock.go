08b8dbdd9be1bd71173378e6a4849216ac3a618e
Joaquin Nicolas <joako.n96@gmail.com>
2018-12-06 12:20:43 -0300

    Bug fixes go stock module

--- stock.go
+++ stock.go
@@ -16,22 +16,24 @@ func StockHandler(c *gin.Context)  {
 		c.JSON(http.StatusBadRequest, gin.H {
 			"error": "symbols must be defined",
 		})
+		return
 	}
 
 	splittedSymbols := strings.Split(symbols, ",")
 
-	data1, err := chart(&http.Client{
+	httpClient := http.Client {
 		Timeout: 5 * time.Second,
-	}, splittedSymbols[0], dataRange)
+	}
+	data1, err := chart(&httpClient, splittedSymbols[0], dataRange)
+	var data2 []*Chart
+	var data3 []*Chart
 
-	data2, err := chart(&http.Client{
-		Timeout: 5 * time.Second,
-	}, splittedSymbols[1], dataRange)
-
-	data3, err := chart(&http.Client{
-		Timeout: 5 * time.Second,
-	}, splittedSymbols[2], dataRange)
-
+	if len(splittedSymbols) > 1{
+		data2, err = chart(&httpClient, splittedSymbols[1], dataRange)
+	}
+	if len(splittedSymbols) > 2 {
+		data3, err = chart(&httpClient, splittedSymbols[2], dataRange)
+	}
 
 	if err != nil {
 		c.JSON(http.StatusForbidden, gin.H {
@@ -40,10 +42,17 @@ func StockHandler(c *gin.Context)  {
 		return
 	}
 
-	result := make([][]*Chart, 3)
+	result := make([][]*Chart, 1)
 	result[0] = data1
-	result[1] = data2
-	result[2] = data3
+
+	if len(data2) > 0 {
+		result = append(result, data2)
+	}
+	
+	if len(data3) > 0 {
+		result = append(result, data3)
+	}
+	
 
 	c.JSON(http.StatusOK, result)
 }
